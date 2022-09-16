7f671c35111bac05d06ccb3c3dea2beae6358cbf
neem92 <nefiespitia@gmail.com>
2022-09-16 15:04:20 +0200

    Scaffold color box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> color generator </h1>
+      <div
+        style={{
+          backgroundColor: 'green ',
+          width: 350,
+          height: 350,
+          border: '3px solid red',
+          fontSize: 80,
+        }}
+      >
+        #12fddgt
+      </div>
+
+      <button> generate </button>
     </div>
   );
 }
-
-export default App;
